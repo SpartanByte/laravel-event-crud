73d2019204af2cdef2aa8ecb097c6c054024f63f
Brian Wardwell <briwar10@outlook.com>
2025-06-15 17:08:15 -0500

    create Event factory and migration

---
+++ database/migrations/2025_06_15_213305_create_events_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->date('event_date'); // <-- Use 'date' for date only
+            $table->string("artist_name");
+            $table->string("venue");
+            $table->string('city_state');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
